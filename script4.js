a28729c81f4b1e0b9957b892d8a4c450f878d1cd
Grzegorz Kolek <gkolek@gazeta.pl>
2018-11-14 10:41:19 +0100

    4.2 hh-mm-ss displayed

--- script4.js
+++ script4.js
@@ -11,21 +11,22 @@
 // console.log(hours);
 // console.log(minutes);
 
-var myVar = setInterval(myTimer, 1000);
-
-function myTimer() {
+var myVar = setInterval(myTimerH, 1000);
+function myTimerH() {
         var date = new Date();
         var hours = date.getHours();
         document.getElementById("fig2").innerHTML = hours;
 }
 
-function myTimer() {
+var myVar = setInterval(myTimerM, 1000);
+function myTimerM() {
         var date = new Date();
         var minutes = date.getMinutes();
         document.getElementById("fig4").innerHTML = minutes;
 }
 
-function myTimer() {
+var myVar = setInterval(myTimerS, 1000);
+function myTimerS() {
         var date = new Date();
         var seconds = date.getSeconds();
         document.getElementById("fig6").innerHTML = seconds;
